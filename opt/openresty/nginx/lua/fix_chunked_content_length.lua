58134f7d478d7d280c902bcea4dba0acf15078f6
Ben Wilber <benwilber@gmail.com>
2015-08-21 15:09:25 -0400

    add link to github issue

--- opt/openresty/nginx/lua/fix_chunked_content_length.lua
+++ opt/openresty/nginx/lua/fix_chunked_content_length.lua
@@ -1,3 +1,4 @@
+--  https://github.com/kaltura/nginx-vod-module/issues/131
 if not ngx.header.content_length and ngx.header.content_range then
   local start_byte, end_byte = ngx.header.content_range:match("bytes (%d+)-(%d+)/%d+")
   if start_byte and end_byte then
